c835490417b815132dde4319b9d075c5cee89475
bonialeart <bonialea@gmail.com>
2026-06-07 21:56:12 -0400

    chore: remove debug log files and update .gitignore to ignore them

--- scripts/debug_run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=C:\Qt\Tools\CMake_64\bin;C:\Qt\6.11.1\mingw_64\bin;C:\Qt\Tools\mingw1310_64\bin;C:\Qt\Tools\Ninja;%PATH%
-echo Running KromoStudio... > run_log.txt
-build_mingw\KromoStudio.exe >> run_log.txt 2>&1
-echo Exit Code: %ERRORLEVEL% >> run_log.txt
